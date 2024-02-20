31bd07089dd137e13ca397d650c1ef6e6ab56cb0
daniel-hortencio <hortencio.daniel@gmail.com>
2024-02-20 11:40:50 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const handleInstallClick = () => {
+    // Verificar se o navegador suporta a instalação de PWA
+    if ("serviceWorker" in navigator && "InstallEvent" in window) {
+      // Verificar se já não está instalado
+      if (!window.matchMedia("(display-mode: standalone)").matches) {
+        // Exibir a opção de instalar o PWA
+        window.prompt(
+          'Para instalar o aplicativo no seu celular, toque em "Adicionar à tela inicial".'
+        );
+      }
+    } else {
+      alert("Seu navegador não suporta a instalação de Progressive Web Apps.");
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        <button
+          style={{
+            background: "white",
+            color: "#252525",
+            fontWeight: 500,
+            fontSize: "1.25rem",
+            padding: "1rem",
+            borderRadius: ".5rem",
+          }}
+          onClick={handleInstallClick}
         >
-          Learn React
-        </a>
+          Instale no seu celular
+        </button>
       </header>
     </div>
   );
